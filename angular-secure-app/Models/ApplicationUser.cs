90fe827ef066abb6d318f146f420bc6e83c0b895
Muhammad Hassan <hsn121121@gmail.com>
2020-01-14 12:08:03 -0800

    pushed repository

---
+++ angular-secure-app/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angular_secure_app.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
